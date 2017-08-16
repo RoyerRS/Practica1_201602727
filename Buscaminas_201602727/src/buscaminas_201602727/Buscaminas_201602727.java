4bfa99b22668896a6e8c42e1a33cde7e58056a13
Dell <Dell@DESKTOP-V852C49>
2017-08-15 23:59:08 -0600

    3Proyecto1_201602727

--- Buscaminas_201602727/src/buscaminas_201602727/Buscaminas_201602727.java
+++ Buscaminas_201602727/src/buscaminas_201602727/Buscaminas_201602727.java
@@ -1,21 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package buscaminas_201602727;
 
-/**
- *
- * @author Dell
- */
+import java.util.Scanner;
+
 public class Buscaminas_201602727 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        
+      Scanner o = new Scanner(System.in);
+        int b;    
+        do{
+        System.out.println("1.Nombre:Rovin Royer Riquiac Salvador");
+        System.out.println("2.Carne:201602727");
+        System.out.println("3.IPC1 A Practica 1");
+        System.out.println("4.Buscaminas");
+         b = o.nextInt();
+         }while(b>4); 
+   switch(b){
+            case 1:
+                clase1 in = new clase1();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+       
+        }
+}
 }
---
+++ Buscaminas_201602727/src/buscaminas_201602727/clase1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas_201602727;
+
+/**
+ *
+ * @author Dell
+ */
+public class clase1 {
+ 
+ public static void main(String[] args){
+ 
+     System.out.println("BUSCAMINAS!!!");
+     System.out.println("1.Principiante");
+     System.out.println("2.Intermedio");
+     System.out.println("3.Experto");
+     System.out.println("Salir");
+ 
+ }
+}
